4fd6563ea9605dc320e85d74e7a8f9fda7b5f2ea
Andy Pratama <andypratama1211@gmail.com>
2026-07-17 22:46:38 +0800

    fix: export testimonials from resources index

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -7,6 +7,7 @@ export {
   about,
   work,
   gallery,
+  testimonials,
 } from "./content";
 
 export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle } from "./once-ui.config";
